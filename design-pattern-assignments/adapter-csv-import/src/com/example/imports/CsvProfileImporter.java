5880e3f717b63ecd3fd3c2d6af7d754abbd0dfe0
kauxp <paramjeetkaur4326@gmail.com>
2025-09-06 20:45:39 +0530

    Added the solution to adapter-csv-import

---
+++ design-pattern-assignments/adapter-csv-import/src/com/example/imports/CsvProfileImporter.java
@@ -0,0 +1,23 @@
+package com.example.imports;
+
+import java.nio.file.Path;
+import java.util.List;
+
+public class CsvProfileImporter implements ProfileImporter {
+    private NaiveCsvReader cvR;
+    private ProfileService pS;
+    public CsvProfileImporter(NaiveCsvReader cvR, ProfileService pS) {
+        this.cvR = cvR;
+        this.pS = pS;
+    }
+    @Override
+    public int importFrom(Path csvFile) {
+        List<String[]> rows = cvR.read(csvFile);
+        for (int i = 1; i < rows.size(); i++) {
+            String[] row = rows.get(i);
+            pS.createProfile(row[0], row[1], row[2]);
+        }
+        return rows.size() - 1; 
+    }
+    
+}
